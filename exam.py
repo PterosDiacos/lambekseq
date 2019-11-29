28e5a0ebfefc3c4b0b4fdb7e958578bde48a10f3
PteroDiacos <pterosdiacos@outlook.com>
2019-11-29 16:43:44 -0500

    update

--- exam.py
+++
@@ -1,5 +0,0 @@
-con, *pres = 's', 's/(np\\s)', 'vt', 's/(np\\s)'
-con, *pres = 's', 'qnp', '(s/(np\\s))\\s'
-con, *pres = '(s/(np\\s))\\s', 'vt', 'qnp'
-
-con, *pres = 's', 'qnp', 'vt', 'qnp', 'inv', 'qnp'
