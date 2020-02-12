e415d3b754bbbd3a563a410121621de4e96c8ebf
PteroDiacos <pterosdiacos@outlook.com>
2020-02-11 22:56:05 -0500

    support dspLinks

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from cindex import indexToken
 from cmll import ProofNet
 from noprodall import findproof, parseProof
 from cntccg import Cntccg
+import displace as dsp
 
 
+LinkSearch = []
+def registerLinkSearch(f):
+    LinkSearch.append(f)
+    return f
+
+
+@registerLinkSearch
 def pnLinks(con: str, pres: list):
     (con, *pres), _ = indexToken(con, pres)
     pn = ProofNet.fromLambekSeq(con, pres)
@@ -18,6 +26,20 @@ def pnLinks(con: str, pres: list):
     return pn.proofCount
 
 
+@registerLinkSearch
+def noprodLinks(con, pres):
+    (con, *pres), _ = indexToken(con, pres)
+    proofs = findproof(con, *pres)
+    links = parseProof(proofs)
+    if links:
+        print('%s\n%s <= %s\n' % ('-' * 10, con, ' '.join(pres)))
+        print(*links, sep='\n', end='\n\n')
+        print('Total: %d\n' % len(links))
+    
+    return len(links)
+
+
+@registerLinkSearch
 def ccgLinks(con, pres):
     (con, *pres), _ = indexToken(con, pres)
     ccg = Cntccg(pres)
@@ -30,16 +52,17 @@ def ccgLinks(con, pres):
     return ccg.proofCount(con)
 
 
-def noprodLinks(con, pres):
-    (con, *pres), _ = indexToken(con, pres)
-    proofs = findproof(con, *pres)
+@registerLinkSearch
+def dspLinks(con, pres):
+    con, *pres = dsp.indexTokens(con, pres)
+    proofs = dsp.findproof(con, *pres)
     links = parseProof(proofs)
     if links:
         print('%s\n%s <= %s\n' % ('-' * 10, con, ' '.join(pres)))
         print(*links, sep='\n', end='\n\n')
         print('Total: %d\n' % len(links))
     
-    return len(links)
+    return len(links)    
 
 
 def deAbbr(con: str, pres: list, abbr: dict):
@@ -59,8 +82,8 @@ if __name__ == '__main__':
     con, *pres = json.load(open('input.json'))[0]
     abbr = json.load(open('abbr.json'))
 
-    n = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    f = [pnLinks, noprodLinks, ccgLinks][n]
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 3
+    f = LinkSearch[n]
     f.count = 0
     for con, pres in deAbbr(con, pres, abbr):
         f.count += f(con, pres)
