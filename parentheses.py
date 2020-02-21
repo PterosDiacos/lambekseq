be61784b76c5994bcf869edd77e514a0fa958d85
PteroDiacos <pterosdiacos@outlook.com>
2020-02-20 22:55:40 -0500

    move to here utilities from lambek.py; bipart supports slash modals

--- parentheses.py
+++ parentheses.py
@@ -1,3 +1,8 @@
+'''Utilities for handling category terms.
+'''
+import re
+
+
 def isatomic(s: str, conn={'/', '\\'}):
     '''Check if `s` contains connectives.'''
     return not any(c in s for c in conn)
@@ -31,20 +36,88 @@ def stripparentheses(s: str, leftPr='(', rightPr=')'):
         return s
 
 
-def bipart(s: str, 
-           leftPr='(', rightPr=')',
-           conn={'/', '\\'},
-           noComma=False):
+def bipart(s: str, leftPr='(', rightPr=')',
+           conn={'/', '\\'}, noComma=False,
+           connMod=set(), withMod=False):
     '''Break a non-atomic `s` into slashes, the left and right components. 
-    The latter two are lists if `noComma` is `False`.'''
+    Both components are lists of strings if `noComma` is `False`.
+    Slashes can be followed by a modal specifier in `connMod`.
+    Return the modal specifier if `withMod` is `True`.'''
     s = stripparentheses(s)
     count = 0
+
     for i in range(len(s)):
-        if s[i] == leftPr: count += 1
-        elif s[i] == rightPr: count -= 1
+        if s[i] == leftPr:
+            count += 1
+        elif s[i] == rightPr:
+            count -= 1
         elif count == 0 and s[i] in conn:
-            left, right = stripparentheses(s[:i]), stripparentheses(s[i + 1:]) 
+            if s[i + 1] in connMod:
+                j = i + 2
+                smod = s[i + 1]
+            else:
+                j = i + 1
+                smod = None
+
+            left, right = stripparentheses(s[:i]), stripparentheses(s[j:]) 
             left, right = commaSplit(left), commaSplit(right)
-            if noComma:
-                left, right = left.pop(), right.pop()
-            return s[i], left, right
+            if noComma: left, right = left.pop(), right.pop()
+            
+            if withMod:
+                return s[i], smod, left, right
+            else:
+                return s[i], left, right
+
+
+def addHypo(x, slash, hypo):
+    if slash is None:
+        return x
+
+    x = x if isatomic(x) else '(%s)' % x
+    hypo = hypo if isatomic(hypo) else '(%s)' % hypo
+
+    if slash == '/':
+        return '%s/%s' % (x, hypo)
+    else:
+        return '%s\\%s' % (hypo, x)
+
+
+def unslash(x:str):
+    '''No commas in `x`.'''
+    xlist = [(x, None, None)]
+
+    while not isatomic(xlist[-1][0]):
+        xslash, xleft, xright = bipart(xlist[-1][0], noComma=True)
+        if xslash == '/':
+            xlist.append((xleft, '/', xright))
+        else:
+            xlist.append((xright, '\\', xleft))
+
+    return xlist
+
+
+def catIden(x:str, y:str) -> (bool, set):
+    '''No commas in `x` and `y`.'''
+    atomCount = int(isatomic(x)) + int(isatomic(y))    
+    if atomCount == 2:
+        return atomicIden(x, y), {tuple(sorted({x, y}))}
+         
+    elif atomCount == 0:
+        xslash, xleft, xright = bipart(x, noComma=True)
+        yslash, yleft, yright = bipart(y, noComma=True)
+        
+        if xslash == yslash:
+            leftIden, leftPairs = catIden(xleft, yleft)
+            rightIden, rightPairs = catIden(xright, yright)
+            return leftIden and rightIden, leftPairs | rightPairs
+
+    return False, set()
+
+
+def atomicIden(x: str, y: str, 
+               pattern=re.compile(r'(?:\A|\()(?:\W*)([a-zA-Z]+)_?(\d*)(?:\Z|\))'), 
+               indexIden=False):
+    '''Check if `x` equals `y` (up to indexation).'''
+    x, x_i = pattern.search(x).groups()
+    y, y_i = pattern.search(y).groups()
+    return x == y and (not indexIden or x_i == y_i)
